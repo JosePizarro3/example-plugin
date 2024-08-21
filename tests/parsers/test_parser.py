e17704df92691a3e5bdd42bd6c152cc0537d7124
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-08-21 10:57:55 +0200

    Fix testing import

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,12 +1,10 @@
 import logging
 
 from nomad.datamodel import EntryArchive
-from nomad_nomad_parser_vasp.parsers.myparser import MyParser
+from nomad_parser_vasp.parsers.parser import VasprunXMLParser
 
 
 def test_parse_file():
-    parser = MyParser()
+    parser = VasprunXMLParser()
     archive = EntryArchive()
     parser.parse('tests/data/example.out', archive, logging.getLogger())
-
-    assert archive.results.material.elements == ['H', 'O']

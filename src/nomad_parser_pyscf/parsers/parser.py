b718a50efa32db16c8f3a2742c6590360db3e7a9
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-08-21 16:25:22 +0200

    Add schema

--- src/nomad_parser_pyscf/parsers/parser.py
+++ src/nomad_parser_pyscf/parsers/parser.py
@@ -11,12 +11,23 @@ if TYPE_CHECKING:
     )
 
 from nomad.config import config
-from nomad.datamodel.results import Material, Results
 from nomad.parsing.parser import MatchingParser
+from nomad.parsing.file_parser import Quantity as ParsedQuantity, TextParser
+from nomad_simulations.schema_packages.general import Simulation, Program
+
 
 configuration = config.get_plugin_entry_point(
     'nomad_parser_pyscf.parsers:pyscf_entry_point'
 )
+
+
+class LogParser(TextParser):
+    def init_quantities(self):
+        self._quantities = [
+            ParsedQuantity(
+                'program_version', r'PySCF *version *([\d\.]+)', repeats=False
+            )
+        ]
 
 
 class PySCFParser(MatchingParser):
@@ -27,4 +38,15 @@ class PySCFParser(MatchingParser):
         logger: 'BoundLogger',
         child_archives: dict[str, 'EntryArchive'] = None,
     ) -> None:
-        print('Hello world!')
+
+        log_parser = LogParser(mainfile=mainfile, logger=logger)
+
+        simulation = Simulation()
+        program = Program(
+            name='PySCF', version=log_parser.get('program_version')
+        )
+        simulation.program = program
+
+        # Add the `Simulation` activity to the `archive`
+        archive.data = simulation
+
